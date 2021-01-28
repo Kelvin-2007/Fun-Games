e85f14bb9b03c3e28e9bbbfb9c85e27485e79a61
Kelvin-2007 <72763850+Kelvin-2007@users.noreply.github.com>
2021-01-29 03:37:56 +0530

    Update Speed Calculator.py

--- Speed Calculator.py
+++ Speed Calculator.py
@@ -1,30 +1,23 @@
 from time import perf_counter
 from pyautogui import prompt, confirm, alert
-from matplotlib import pyplot
 
 results = []
-y = []
 
-ticks = 0
 while True:
     input("Start")
     start = perf_counter()
     msg = prompt("Type something", "Prompt")
     if msg == "quit":
-        confirm(f"Your record is {max(results)} keys/s", "Record")
-        pyplot.plot(results, y)
-        pyplot.show()
-    seconds = perf_counter() - start
+        confirm(f"Your record is {max(results)} keys/m", "Record")
+    seconds = perf_counter() - start*60
     speed = round(len(msg) / seconds)
     if speed < 3:
-        alert(f"Your current speed is {speed}", "Your speed is not good")
+        alert(f"Your current speed is {speed} keys/m", "Your speed is not good")
     else:
-        alert(f"Your current speed is {speed}", "Your speed is good")
+        alert(f"Your current speed is {speed} keys/m", "Your speed is good")
     try:
         if speed > max(results):
             alert("And that's a new highscore", "Highscore")
     except ValueError:
         confirm("And that's a new highscore", "Highscore")
     results.append(len(msg) / seconds)
-    y.append(ticks)
-    ticks += 1
